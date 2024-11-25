5aecb98205f13a3370b3103bf420bc8b6963f1e7
Dung1324\LVD <luuvandung1324@gmail.com>
2024-11-25 21:09:24 +0700

    up ex1

--- BTTH/2511/ex1/user.php
+++ BTTH/2511/ex1/user.php
@@ -1,16 +1,31 @@
 <?php global $flowers;
-include('itemheader.php'); ?>
-<?php include('flowers.php');
+include('itemheader.php');
 ?>
+<?php include('flowers.php'); ?>
 
-<div class="flower-list">
-    <?php foreach ($flowers as $flower): ?>
-        <div class="flower-item">
-            <h2><?= $flower['name'] ?></h2>
-            <img src="<?= $flower['image'] ?>" alt="<?= $flower['name'] ?>" >
-            <p><?= $flower['moTa'] ?></p>
+<div class="container-xl">
+    <div class="table-responsive">
+        <div class="table-wrapper">
+            <table class="table table-striped table-hover">
+                <thead>
+                <tr>
+                    <th>Tên</th>
+                    <th>Mô tả</th>
+                    <th>Ảnh</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($flowers as $flower):?>
+                    <tr>
+                        <td><?= htmlspecialchars($flower['name']) ?></td>
+                        <td><?= htmlspecialchars($flower['moTa']) ?></td>
+                        <td><img src="<?= $flower['image'] ?>" alt="<?= $flower['name'] ?> "width="100" ></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
         </div>
-    <?php endforeach; ?>
+    </div>
 </div>
 
 <?php include('itemfooter.php'); ?>
